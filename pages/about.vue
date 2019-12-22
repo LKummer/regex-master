95128733af60a102b95aee4dd3a953c16a55b0dc
Lior Kummer <LKummer41@gmail.com>
2019-12-23 00:03:57 +0200

    Add about page content.

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,65 @@
 <template>
-  <v-card>
-    <v-card-text>
-      About this web app.
-    </v-card-text>
-  </v-card>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-card>
+      <v-card-text>
+        <p>
+          When looking around for a similar tool, it seemed like regex testers
+          that match against multiple inputs are rare. So I made one myself.
+        </p>
+        <p>
+          This progressive web app is powered by
+          <v-chip
+            link
+            small
+            href="https://vuejs.org/"
+          >
+            Vue
+          </v-chip>
+          and built using
+          <v-chip
+            link
+            small
+            href="https://nuxtjs.org/"
+          >
+            Nuxt
+          </v-chip>
+          and
+          <v-chip
+            link
+            small
+            href="https://vuetifyjs.com/"
+          >
+            Vuetify
+          </v-chip>
+          . It is free and open source with no advertisements or trackers.
+        </p>
+        <v-divider />
+        <p class="mt-4 text-center">
+          Check out my Github!
+          <v-chip
+            link
+            href="https://github.com/LKummer/"
+          >
+            <v-icon left>
+              mdi-github-circle
+            </v-icon>
+            Lior Kummer
+          </v-chip>
+        </p>
+        <p class="mb-0 text-center">
+          Made in the Middle East with
+          <v-icon
+            large
+            color="red"
+          >
+            mdi-heart
+          </v-icon>
+        </p>
+      </v-card-text>
+    </v-card>
+  </v-layout>
 </template>
