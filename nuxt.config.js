e32ac531072270478b2b3dc72fdc710aada86786
Lior Kummer <LKummer41@gmail.com>
2019-12-24 00:01:18 +0200

    Refactor color theme.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-// import colors from 'vuetify/es5/util/colors'
+import colors from 'vuetify/es5/util/colors'
 
 export default {
   mode: 'universal',
@@ -53,20 +53,13 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      /*
-      dark: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+        light: {
+          primary: '#0f4c81',
+          info: '#1569b2',
+          error: colors.deepOrange.darken1
         }
       }
-      */
     }
   },
   /*
