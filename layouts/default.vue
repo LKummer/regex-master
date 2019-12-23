e2276cde041bd58a704b8e41f97a61259cec0d1b
Lior Kummer <LKummer41@gmail.com>
2019-12-23 20:27:07 +0200

    Refactor default template and add a hamburger menu for extra small screen sizes.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,75 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      disable-resize-watcher
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, index) of items"
+          :key="index"
+          :to="item.to"
+        >
+          <v-list-item-title>
+            {{ item.title }}
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item
+          :href="source.to"
+        >
+          <v-list-item-title>
+            <v-icon left>
+              {{ source.icon }}
+            </v-icon>
+            {{ source.title }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-content>
       <v-toolbar
         dark
         color="primary"
       >
+        <v-btn
+          @click="drawer = !drawer"
+          text
+          icon
+          class="d-flex d-sm-none"
+        >
+          <v-icon>
+            mdi-menu
+          </v-icon>
+        </v-btn>
         <v-toolbar-title
           class="mr-4"
         >
           Regex Master
         </v-toolbar-title>
-        <v-toolbar-items>
+        <v-toolbar-items
+          class="d-none d-sm-flex"
+        >
           <v-btn
-            to="/"
+            v-for="(item, index) of items"
+            :key="index"
+            :to="item.to"
             text
           >
-            Home
+            {{ item.title }}
           </v-btn>
         </v-toolbar-items>
         <v-spacer />
-        <v-toolbar-items>
+        <v-toolbar-items
+          class="d-none d-sm-flex"
+        >
           <v-btn
-            to="/about"
+            :href="source.to"
             text
           >
-            About
-          </v-btn>
-          <v-btn text>
-            Star on Github!
-            <v-icon>mdi-github-circle</v-icon>
+            {{ source.title }}
+            <v-icon>{{ source.icon }}</v-icon>
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
@@ -45,25 +86,22 @@
 export default {
   data () {
     return {
-      clipped: false,
       drawer: false,
-      fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Matcher',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'About',
+          to: '/about'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      source: {
+        title: 'Star on Github!',
+        to: 'https://github.com/LKummer/',
+        icon: 'mdi-github-circle'
+      }
     }
   }
 }
